b4e36130b59e568ba611a96d52a4be4c1f1665a4
felixmachan <126292501+felixmachan@users.noreply.github.com>
2024-02-09 14:38:56 +0100

    Update scoreboard.py
    Added highscore feature requirements for Scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score =  0
+        with open("data.txt") as hs:
+            self.high_score = int(hs.read())
         self.pencolor("white")
         self.penup()
         self.setx(0)
@@ -13,16 +15,24 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}",align=ALIGNMENT,  font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}",align=ALIGNMENT,  font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as hs:
+                hs.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
 
